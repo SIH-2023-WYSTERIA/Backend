b6e30f079f081dcd99c3c4e59abdc8401f5eb3cd
hari vishwanath <mailtohari134@gmail.com>
2023-09-14 00:15:34 +0530

    .

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-# Use an official Python runtime as a parent image
-FROM python:3.8-slim
+# Use an official Ubuntu image as a parent image
+FROM ubuntu:20.04
 
 # Set the working directory to /app
 WORKDIR /app
@@ -7,11 +7,38 @@ WORKDIR /app
 # Copy the current directory contents into the container at /app
 COPY . /app
 
+# Set the DEBIAN_FRONTEND environment variable to noninteractive
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Install Python and pip
+RUN apt-get update && apt-get install -y python3 python3-pip
+
+# Install GPU dependencies (NVIDIA CUDA Toolkit and cuDNN)
+RUN apt-get install -y --no-install-recommends \
+    software-properties-common \
+    && rm -rf /var/lib/apt/lists/* \
+    && add-apt-repository multiverse \
+    && apt-get update \
+    && apt-get install -y nvidia-cuda-toolkit ffmpeg git \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
 # Install any needed packages specified in requirements.txt
-RUN pip install --trusted-host pypi.python.org -r requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
 
 # Make port 5000 available to the world outside this container
 EXPOSE 5000
 
 # Run app.py when the container launches
-CMD ["python", "main.py"]
+CMD ["python3", "main.py"]
+
+
+
+
+################################
+# BUILD DETAILS FOR PRODUCTION #
+################################
+# FIRST INSTALL CUDA AND NVIDIA DRIVERS
+# RUN docker run --rm --gpus all nvidia/cuda:11.0.3-base-ubuntu20.04 nvidia-smi
+# IF IT RUNS PROPERLY AND OUTPUT IS SHOWN THEN PROCEED
+# RUN docker build -t sih .
--- services/models/model_service.py
+++ services/models/model_service.py
@@ -1,3 +1,4 @@
+import shutil
 from typing import List, Optional, Union
 
 import numpy as np
@@ -9,25 +10,42 @@ from transformers import pipeline
 from transformers.pipelines.audio_utils import ffmpeg_read
 import torchaudio
 import io
+from pydub import AudioSegment
 
 import torch
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 from pyannote.audio import Pipeline
 
 diarization_pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization@2.1", use_auth_token="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"
+    "pyannote/speaker-diarization@2.1",
+    use_auth_token="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",
 )
 
-from transformers import WhisperProcessor, WhisperForConditionalGeneration, WhisperFeatureExtractor, WhisperTokenizer, WhisperProcessor
+from transformers import (
+    WhisperProcessor,
+    WhisperForConditionalGeneration,
+    WhisperFeatureExtractor,
+    WhisperTokenizer,
+    WhisperProcessor,
+)
 from datasets import Audio, load_dataset
 
 processor = WhisperProcessor.from_pretrained("Venkatesh4342/whisper-small-en-hi")
-forced_decoder_ids = processor.get_decoder_prompt_ids(language="english", task="transcribe")
+forced_decoder_ids = processor.get_decoder_prompt_ids(
+    language="english", task="transcribe"
+)
 
-tokenizer = WhisperTokenizer.from_pretrained("Venkatesh4342/whisper-small-en-hi", language="english", task="transcribe")
-model = WhisperForConditionalGeneration.from_pretrained("Venkatesh4342/whisper-small-en-hi").to(device)
-feature_extractor = WhisperFeatureExtractor.from_pretrained("Venkatesh4342/whisper-small-en-hi")
+tokenizer = WhisperTokenizer.from_pretrained(
+    "Venkatesh4342/whisper-small-en-hi", language="english", task="transcribe"
+)
+model = WhisperForConditionalGeneration.from_pretrained(
+    "Venkatesh4342/whisper-small-en-hi"
+).to(device)
+feature_extractor = WhisperFeatureExtractor.from_pretrained(
+    "Venkatesh4342/whisper-small-en-hi"
+)
 
 asr_pipeline = pipeline(
     "automatic-speech-recognition",
@@ -35,10 +53,11 @@ asr_pipeline = pipeline(
     feature_extractor=feature_extractor,
     tokenizer=tokenizer,
     device=device,
-    generate_kwargs={"forced_decoder_ids": forced_decoder_ids}
+    generate_kwargs={"forced_decoder_ids": forced_decoder_ids},
 )
 
 sampling_rate = asr_pipeline.feature_extractor.sampling_rate
+
 
 def preprocess(inputs):
     if isinstance(inputs, str):
@@ -83,123 +102,121 @@ def preprocess(inputs):
 
     return inputs, diarizer_inputs
 
-def speech2text_pipeline(input,diarization_pipeline,asr_pipeline):
-  inputs, diarizer_inputs = preprocess(input)
-  diarization_pipeline.to(torch.device(device))
-  diarizer_output = diarization_pipeline({"waveform": diarizer_inputs, "sample_rate": sampling_rate}, num_speakers=2)
-  segments = []
-  for segment, track, label in diarizer_output.itertracks(yield_label=True):
-      segments.append({'segment': {'start': segment.start, 'end': segment.end},
-                             'track': track,
-                             'label': label})
 
-  new_segments = []
-  prev_segment = cur_segment = segments[0]
-
-  for i in range(1, len(segments)):
-    cur_segment = segments[i]
-    if cur_segment["label"] != prev_segment["label"] and i < len(segments):
-        new_segments.append(
+def speech2text_pipeline(input, diarization_pipeline, asr_pipeline):
+    inputs, diarizer_inputs = preprocess(input)
+    diarization_pipeline.to(torch.device(device))
+    diarizer_output = diarization_pipeline(
+        {"waveform": diarizer_inputs, "sample_rate": sampling_rate}, num_speakers=2
+    )
+    segments = []
+    for segment, track, label in diarizer_output.itertracks(yield_label=True):
+        segments.append(
             {
-                "segment": {"start": prev_segment["segment"]["start"], "end": cur_segment["segment"]["start"]},
-                "speaker": prev_segment["label"],
+                "segment": {"start": segment.start, "end": segment.end},
+                "track": track,
+                "label": label,
             }
         )
-        prev_segment = segments[i]
 
-  new_segments.append(
-    {
-        "segment": {"start": prev_segment["segment"]["start"], "end": cur_segment["segment"]["end"]},
-        "speaker": prev_segment["label"],
-    }
-  )
+    new_segments = []
+    prev_segment = cur_segment = segments[0]
 
-  asr_out = asr_pipeline(
-            {"array": inputs, "sampling_rate": sampling_rate},
-            chunk_length_s=30,
-            return_timestamps=True)
+    for i in range(1, len(segments)):
+        cur_segment = segments[i]
+        if cur_segment["label"] != prev_segment["label"] and i < len(segments):
+            new_segments.append(
+                {
+                    "segment": {
+                        "start": prev_segment["segment"]["start"],
+                        "end": cur_segment["segment"]["start"],
+                    },
+                    "speaker": prev_segment["label"],
+                }
+            )
+            prev_segment = segments[i]
 
-  transcript = asr_out["chunks"]
-  end_timestamps = np.array([chunk["timestamp"][-1] for chunk in transcript])
-  if end_timestamps[-1] == None:
-    end_timestamps[-1] = new_segments[-1]["segment"]["end"]
+    chunks = []
+    for segment in new_segments:
+        chunks.append(segment["segment"])
 
-  group_by_speaker=True
+    def chunk_audio(input_file, output_folder, chunks):
+        audio = AudioSegment.from_file(input_file)
+        os.makedirs(output_folder, exist_ok=True)
 
-  segmented_preds = []
-  for segment in new_segments:
-    end_time = segment["segment"]["end"]
-    try:
-       upto_idx = np.argmin(np.abs(end_timestamps - end_time))
-    except ValueError:
-        continue
-    if group_by_speaker:
-        segmented_preds.append(
-           {
-                "speaker": segment["speaker"],
-                "text": "".join([chunk["text"] for chunk in transcript[: upto_idx + 1]]),
-                "timestamp": (transcript[0]["timestamp"][0], transcript[upto_idx]["timestamp"][1]),
-          }
+        for i, chunk in enumerate(chunks):
+            start_time = chunk["start"] * 1000
+            end_time = chunk["end"] * 1000
+            chunk_audio = audio[start_time:end_time]
+            output_file = os.path.join(output_folder, f"chunk_{i}.mp3")
+            chunk_audio.export(output_file, format="wav")
+
+        print(f"Audio file '{input_file}' has been chunked into {len(chunks)} chunks.")
+
+    input_file = input
+    output_folder = "folder"
+
+    chunk_audio(input_file, output_folder, chunks)
+
+    res = []
+    for i in range(len(new_segments)):
+        inputs, diarizer_inputs = preprocess(os.path.join("folder", f"chunk_{i}.mp3"))
+        asr = asr_pipeline(
+            {"array": inputs, "sampling_rate": sampling_rate}, chunk_length_s=30
         )
-    else:
-         for i in range(upto_idx + 1):
-               segmented_preds.append({"speaker": segment["speaker"], **transcript[i]})
-    transcript = transcript[upto_idx + 1 :]
-    end_timestamps = end_timestamps[upto_idx + 1 :]
+        res.append(asr)
 
-  conv = []
-  for i,sub in enumerate(segmented_preds):
-    if(i %2==0):
-      conv.append(f"helpdesk:{sub['text']}")
-    else:
-      conv.append(f"customer:{sub['text']}")
+    shutil.rmtree("folder")
 
-  return conv
+    conv = []
+    for i, sub in enumerate(res):
+        if i % 2 == 0:
+            conv.append(f"helpdesk:{sub['text']}")
+        else:
+            conv.append(f"customer:{sub['text']}")
+
+    return conv
+
 
 model_name = "Venkatesh4342/bart-samsum"
 
-def summerization_pipeline(s2t_output,model_name):
-  pipe = pipeline("summarization", model=model_name,device=device)
-  pipe_out = pipe("\n".join(s2t_output))
-  return pipe_out[0]["summary_text"]
+
+pipe = pipeline("summarization", model=model_name, device=device)
+
+
+def summerization_pipeline(s2t_output, model_name):
+    pipe_out = pipe("\n".join(s2t_output))
+    return pipe_out[0]["summary_text"]
+
 
 from optimum.onnxruntime import ORTModelForSequenceClassification
 from transformers import AutoTokenizer
 
 quant_model = ORTModelForSequenceClassification.from_pretrained("services/models/model")
-quant_tokenizer = AutoTokenizer.from_pretrained("Venkatesh4342/distilbert-helpdesk-sentiment")
+quant_tokenizer = AutoTokenizer.from_pretrained(
+    "Venkatesh4342/distilbert-helpdesk-sentiment"
+)
 
-def classification(summerized_op, quant_model, quant_tokenizer ):
-   pipe =  pipeline("text-classification", model=quant_model, tokenizer=quant_tokenizer ,device=0)
-   res = pipe(summerized_op)
-   return res
 
-def convert_audio_to_numpy(audio_file):
-    try:
-        # Read the audio data from the FileStorage object
-        audio_data, _ = torchaudio.load(io.BytesIO(audio_file.read()))
+def classification(summerized_op, quant_model, quant_tokenizer):
+    pipe = pipeline(
+        "text-classification", model=quant_model, tokenizer=quant_tokenizer, device=0
+    )
+    res = pipe(summerized_op)
+    return res
 
-        # Check if the audio data is in mono format (single channel)
-        if audio_data.shape[0] != 1:
-            raise ValueError("We expect a single channel audio input")
 
-        # Convert the audio data to a NumPy ndarray
-        audio_data = audio_data.numpy()
-
-        return audio_data
-    except Exception as e:
-        # Handle any exceptions that may occur during audio conversion
-        raise ValueError("Error converting audio to NumPy ndarray: {}".format(str(e)))
-    
 def Model_Inference(file):
     data = {}
-    speech2text_output = speech2text_pipeline(file,diarization_pipeline,asr_pipeline)
-    summerized_output = summerization_pipeline(speech2text_output,model_name)
-    sentiment = classification(summerized_output, quant_model, quant_tokenizer )
+    speech2text_output = speech2text_pipeline(file, diarization_pipeline, asr_pipeline)
+    summerized_output = summerization_pipeline(speech2text_output, model_name)
+    sentiment = classification(summerized_output, quant_model, quant_tokenizer)
 
-    data.update({
-        "summary":summerized_output,
-        "sentiment": sentiment[0]["label"],
-        "score": sentiment[0]["score"]
-    })
+    data.update(
+        {
+            "summary": summerized_output,
+            "sentiment": sentiment[0]["label"],
+            "score": sentiment[0]["score"],
+        }
+    )
     return data
