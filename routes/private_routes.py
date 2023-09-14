81cf9e7a8d44696c860fe4683d8080abead6b6c9
hari vishwanath <mailtohari134@gmail.com>
2023-09-14 15:02:18 +0530

    .

--- routes/private_routes.py
+++ routes/private_routes.py
@@ -3,5 +3,5 @@ from controllers.conversation import SendConversation, GetAllConversations
 
 private_bp = Blueprint('private', __name__,url_prefix='/private')
 
-private_bp.add_url_rule('/send_conversation', view_func=SendConversation.as_view('send_conversation'))
+# private_bp.add_url_rule('/send_conversation', view_func=SendConversation.as_view('send_conversation'))
 private_bp.add_url_rule('/get_all_conversations', view_func=GetAllConversations.as_view('get_all_conversations'))
