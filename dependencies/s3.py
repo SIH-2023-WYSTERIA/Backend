079f6f7762475c24e33347b5d0981f049489fc13
Yash Gupta <108118711+yashpapa6969@users.noreply.github.com>
2024-08-26 02:17:21 +0530

    Update s3.py

--- dependencies/s3.py
+++ dependencies/s3.py
@@ -9,15 +9,15 @@ class S3:
     def __init__(self):
         self.s3 = boto3.client(
             service_name="s3",
-            region_name=os.getenv("AWS_REGION_NAME"),
+            region_name=os.getenv("AWS_BUCKET_REGION"),
             aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
         )
 
     def upload_file(self, file_path, filename):
         try:
             with open(file_path, 'rb') as file:
-                self.s3.upload_fileobj(file, os.getenv('S3_BUCKET_NAME'), filename)
+                self.s3.upload_fileobj(file, os.getenv('AWS_BUCKET_NAME'), filename)
         except Exception as e:
             return False, str(e)
         return True, None
@@ -26,7 +26,7 @@ class S3:
         # Generate a pre-signed URL for the audio file
         presigned_url = self.s3.generate_presigned_url(
             'get_object',
-            Params={'Bucket': os.getenv('S3_BUCKET_NAME'), 'Key': file_key},
+            Params={'Bucket': os.getenv('AWS_BUCKET_NAME'), 'Key': file_key},
             ExpiresIn=31536000  # Set an expiration time for the URL (e.g., 1 hour)
         )
         return presigned_url
