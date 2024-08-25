f04050dcd3d69318b65d080304cc56747931416b
Yash Gupta <108118711+yashpapa6969@users.noreply.github.com>
2024-08-26 01:19:27 +0530

    Update auth.py

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -9,29 +9,11 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
+
 def token_required(role):
     def decorator(f):
-        @wraps(f)
         def decorated(*args, **kwargs):
-            token = request.headers.get('Authorization')
-
-            if not token:
-                return jsonify({'message': 'Token is missing'}), 401
-
-            try:
-                data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-                user_role = data.get('sub')['role']
-                request.environ['decoded_jwt'] = data.get('sub')
-                if user_role != role:
-                    return jsonify({'message': 'Unauthorized'}), 403
-
-            except jwt.ExpiredSignatureError:
-                return jsonify({'message': 'Token has expired'}), 401
-            except jwt.InvalidTokenError:
-                return jsonify({'message': 'Invalid token'}), 401
-
-            
+            # Removed token and role checking logic
             return f(*args, **kwargs)
-
         return decorated
     return decorator
